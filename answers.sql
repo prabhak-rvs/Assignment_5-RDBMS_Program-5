e4609cc86241ed35ff448cbbef558caa42a94ae5
prabhak-rvs <prabhak@rvsgroup.com>
2026-08-21 14:23:12 +0530

    Refactor SQL insert and select statements

--- answers.sql
+++ answers.sql
@@ -1,13 +1,10 @@
 USE CollegeDB;
 
--- Insert the following student records
-
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+INSERT INTO Student
+(StudentID, StudentName, Gender, DepartmentID)
 VALUES
 (1001, 'Arun', 'Male', 101),
 (1002, 'Divya', 'Female', 102),
 (1003, 'Karthik', 'Male', 101);
-
--- Display all student records
 
 SELECT * FROM Student;
